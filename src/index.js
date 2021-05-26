edcfbe4daf7cff7aa18296fe04738225e7604501
lauratamath <laurylevi@hotmail.com>
2021-05-26 00:28:46 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-undef */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <h1>Hello world</h1>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+)
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?)$/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|otf|mp4)$/i,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+}
